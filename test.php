578a531016e26896d616e287f8922dcd2a449d41
Amir Shahmi <ashahmi0406@gmail.com>
2024-12-05 14:01:35 +0800

    extras

--- test.php
+++ test.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php include('includes/header.html'); ?>
 </head>
 <body>
-    <?php
-
-    echo 'Hello World';
-
-    ?>
+    <div class="container mt-4">
+        <?php echo 'Test from php' ?>
+    </div>
 </body>
+<?php include('includes/footer.html'); ?>
 </html>
